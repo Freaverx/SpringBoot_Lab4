38af0f6074265e7df47cdf918565dc29fa4b48a0
Freaverx <deimos.sergei@yandex.ru>
2024-10-15 15:19:15 +0500

    Lab4

---
+++ src/main/java/ru/proskuryakov/MySecondTestAppSpringBoot/service/ModifyRequestService.java
@@ -0,0 +1,8 @@
+package ru.proskuryakov.MySecondTestAppSpringBoot.service;
+
+
+import ru.proskuryakov.MySecondTestAppSpringBoot.model.Request;
+
+public interface ModifyRequestService {
+    void modify(Request request);
+}
